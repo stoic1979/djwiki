73722a2976e8435d5ba30ad86acdc34ce8522760
Navjot Singh <weavebytes@gmail.com>
2015-10-07 00:04:39 +0530

    register models with admin

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from wiki.models import *
+
+admin.site.register(Category)
+admin.site.register(Article)
+admin.site.register(Edit)
