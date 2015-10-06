7d894605832d6e0c330e300e9f2a5e82f40c2b42
Navjot Singh <weavebytes@gmail.com>
2015-10-07 00:46:05 +0530

    added captcha in installed apps

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -39,6 +39,11 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # third party apps
+    'captcha',
+
+    # app for the project
     'wiki',
     'registration',
 )
