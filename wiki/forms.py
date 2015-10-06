07007e564f7bd4de5240336ff690153326a869e7
Navjot Singh <weavebytes@gmail.com>
2015-10-07 00:48:38 +0530

    added category form

--- wiki/forms.py
+++ wiki/forms.py
@@ -2,6 +2,10 @@ from django import forms
 
 from models import Article, Edit
 
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+
 class ArticleForm(forms.ModelForm):
     class Meta:
         model = Article
