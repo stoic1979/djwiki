4878b66c4d96f35beb0a185b6d3d2692049f1acd
Navjot Singh <weavebytes@gmail.com>
2015-10-07 00:38:59 +0530

    added login/logout hadlers

--- wiki/views.py
+++ wiki/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import *
+from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from django.core.context_processors import csrf
 
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.template import RequestContext
 #from django.views.generic.list_detail import object_list
+
+from dashboard.forms import LoginForm
 
 from models import Category, Article, Edit
 from forms import ArticleForm, EditForm
@@ -16,6 +21,53 @@ def home(request):
     """
     c = {'categories': Category.objects.all(), 'request': request}
     return render_to_response('index.html', c, context_instance=RequestContext(request))
+
+def login_page(request):
+    """
+    If user is authenticated, direct them to the next page. 
+    Otherwise, take them to the login page.
+
+    :param request: django HttpRequest
+
+    :return: django HttpResponse 
+    """
+
+    state = ""
+    username = password = ''
+    form = LoginForm()
+
+    #default next page is index page
+    next_page = "/"
+
+    #getting next page in get request
+    if request.GET:
+        next_page = request.GET.get('next')
+
+    if request.POST:
+        form = LoginForm(request.POST) # A form bound to the POST data
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        next_page = request.POST.get('next')
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                state = "You're successfully logged in!"
+                return HttpResponseRedirect(next_page) # Redirect after POST
+            else:
+                state = "Your account is not active, please contact the site admin."
+        else:
+            state = "Your username and/or password were incorrect."
+
+    c = {'state':state, 'username': username, 'form': form, 'next': next_page}
+    c.update(csrf(request)) 
+
+    return render_to_response('auth.html', c)
+
+def logout_page(request):
+    """ Log users out and re-direct them to the main page. """
+    logout(request)
+    return HttpResponseRedirect('/login/', {'request':request})
 
 """
 @login_required
