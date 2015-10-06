fc7e57bcf9e03fd82011903720b72d9becd2c677
Navjot Singh <weavebytes@gmail.com>
2015-10-06 22:05:03 +0530

    add url mappings

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -18,4 +18,19 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 
+        'django.views.generic.list_detail.object_list',
+        {
+            'queryset': Article.published.all(),
+        },
+        name='wiki_article_index'),
+    url(r'^article/(?P<slug>[-\w]+)$', 
+        'django.views.generic.list_detail.object_detail',
+        {
+            'queryset': Article.objects.all(),
+        },
+        name='wiki_article_detail'),
+    url(r'^history/(?P<slug>[-\w]+)$', 'wiki.views.article_history', name='wiki_article_history'),
+    url(r'^add/article$', 'wiki.views.add_article', name='wiki_article_add'),
+    url(r'^edit/article/(?P<slug>[-\w]+)$', 'wiki.views.edit_article', name='wiki_article_edit'),
 ]
